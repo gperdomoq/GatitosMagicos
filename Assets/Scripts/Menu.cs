b00fb6bc0650459dc439b82770131f2834d3ddca
Gabriela Perdomo <123596087+gperdomoq@users.noreply.github.com>
2026-02-28 11:48:22 -0500

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Jugar()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Salir()
     {
-        
+        Debug.Log("Saiendo del juego...");
+        Application.Quit();
     }
 }
